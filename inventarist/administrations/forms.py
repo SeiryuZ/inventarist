c861ed2eb280ed92e076daeb1960ec23a5fa8c6a
Steven Only <SeiryuZ@hotmail.com>
2013-08-06 10:06:45 +0700

    Added search ajax for admin

--- inventarist/administrations/forms.py
+++ inventarist/administrations/forms.py
@@ -1,24 +1,17 @@
 from django import forms
 
+from ajax_select.fields import AutoCompleteSelectField
+
 from apps.products.models import Product
 
 
 class OperationProductForm(forms.Form):
-    product = forms.IntegerField()
+    product = AutoCompleteSelectField('product')
     quantity = forms.IntegerField()
 
     def __init__(self, operation='addition', *args, **kwargs):
         super(OperationProductForm, self).__init__(*args, **kwargs)
         self.operation = operation
-
-    def clean_product(self):
-        product = self.cleaned_data['product']
-        try:
-            product = Product.objects.get(id=product)
-            return product
-        except Product.DoesNotExist:
-            raise forms.ValidationError("Product can't be found")
-        return None
 
     def save(self, *args, **kwargs):
         quantity = self.cleaned_data['quantity']
--- inventarist/settings.py
+++ inventarist/settings.py
@@ -131,6 +131,7 @@ INSTALLED_APPS = (
 
     # 3rd party
     'django_extensions',
+    'ajax_select',
     'south',
 
     # Apps
@@ -139,6 +140,15 @@ INSTALLED_APPS = (
 
 
 )
+
+# define the lookup channels in use on the site
+AJAX_LOOKUP_CHANNELS = {
+    #   pass a dict with the model and the field to search against
+    'product'  : {'model':'products.Product', 'search_field':'name'}
+}
+# magically include jqueryUI/js/css
+AJAX_SELECT_BOOTSTRAP = True
+AJAX_SELECT_INLINES = 'inline'
 
 # A sample logging configuration. The only tangible logging
 # performed by this configuration is to send an email to
